22d2c6cfc438c3f457ed8aac6105fa49776b538c
kobzev_iv <xronosss@mail.ru>
2022-05-05 16:46:12 +0700

    Merge branch 'git-merge'

--- 4_1_1.py
+++ 4_1_1.py
@@ -1,5 +1,5 @@
 import os
-bash_command = ["C:\\Users\\kobzev_iv\\PycharmProjects\\devops-netology", "git status"]
+bash_command = ["cd C:\\Users\\kobzev_iv\\PycharmProjects\\devops-netology", "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
