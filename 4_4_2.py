2cc88e793da1dddecc005c6c8fe4b03c9d881981
kobzev_iv <xronosss@mail.ru>
2022-05-05 17:03:18 +0700

    Merge branch 'git-merge'

---
+++ 4_4_2.py
@@ -0,0 +1,8 @@
+import os
+path = os.path.abspath
+bash_command = [f'cd {path}', "git status"]
+result_os = os.popen(' && '.join(bash_command)).read()
+for result in result_os.split('\n'):
+    if result.find('modified') != -1:
+        prepare_result = result.replace('\tmodified:   ', '')
+        print(prepare_result)
