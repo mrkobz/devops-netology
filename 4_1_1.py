869490c5d1a529da769016d255b1b1ee731ef59c
kobzev_iv <xronosss@mail.ru>
2022-05-05 16:20:40 +0700

    Merge branch 'git-merge'

--- 4_1_1.py
+++ 4_1_1.py
@@ -1,7 +1,6 @@
 import os
-bash_command = ["cd ~/devops-netology", "git status"]
+bash_command = ["C:\\Users\\kobzev_iv\\PycharmProjects\\devops-netology", "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
-is_change = False  # не используется, непонятно зачем нужна. можно удалить эту строку
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
         prepare_result = result.replace('\tmodified:   ', '')
